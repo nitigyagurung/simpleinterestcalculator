4d1e59faef3b468bd8ce1a9bfec3e5962cd843d7
nitigya gurung <grgniti07@gmail.com>
2022-03-21 16:29:37 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import './index.css'
 
-function App() {
+
+const App = () => {
+  const [p, setP] = useState(0)
+  const [t, setT] = useState(0)
+  const [r, setR] = useState(0)
+  const [si, setSi] = useState(0)
+
+  const calculateInterest = ()=>{
+    setSi(p*t*r/100)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='div'>
+      <AppBar position="static">
+        <Toolbar>
+          
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Simple Interest
+          </Typography>
+          
+        </Toolbar>
+      </AppBar>
+      <br/>
+      <TextField onChange={(event) => setP(event.target.value)} id="outlined-basic" label="Enter Principal" variant="outlined" />
+      <br/><br/>
+      <TextField onChange={(event) => setT(event.target.value)} id="outlined-basic" label="Enter Time" variant="outlined" />
+      <br/> <br/>
+      <TextField onChange={(event) => setR(event.target.value)} id="outlined-basic" label="Enter Rate" variant="outlined" />
+      <br/><br/>
+      <Button onClick={()=>{
+        return calculateInterest();
+      }} variant="contained">Calculate</Button>
+      <Typography variant="h6" gutterBottom component="div">
+        Simple Interest is: {si}
+      </Typography>
     </div>
   );
 }
